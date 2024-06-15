9dc1837a00837352706861ead2230528609725ca
Shivd131 <shivdeshpande131@gmail.com>
2024-06-15 13:28:54 +0530

    feat: get details to the profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const profilePage = () => {
     const router = useRouter();
+    const [data, setData] = useState(
+        "nothing"
+    )
+
     const logout = async () => {
         try {
             await axios.get('/api/users/logout');
@@ -15,9 +19,25 @@ const profilePage = () => {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data)
+        setData(res.data.data._id)
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center  min-h-screen py-2">
             <h1>Profile</h1>
+            <p>Details</p>
+            <h2>{data === "nothing" ? "Not found" :
+                <Link className="bg-green-600 p-2 m-2 rounded-md" href={`/profile/${data}`}>
+                    {data}
+                </Link>
+            } </h2>
+            <button onClick={getUserDetails} className="bg-blue-500 hover:bg-blue-700 text-white p-2 my-2 rounded-md">
+                Get User Details
+            </button>
             <hr />
             <button onClick={logout} className="bg-blue-500 hover:bg-blue-700 text-white p-2 my-2 rounded-md">
                 Logout
