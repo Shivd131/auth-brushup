9f59d3fed24eec3cb8424cb78cabdb0fec398522
Shivd131 <shivdeshpande131@gmail.com>
2024-06-15 12:43:22 +0530

    feat: logout functionality and middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,27 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+
 const profilePage = () => {
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get('/api/users/logout');
+            router.push('/login')
+        } catch (error: any) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center  min-h-screen py-2">
             <h1>Profile</h1>
+            <hr />
+            <button onClick={logout} className="bg-blue-500 hover:bg-blue-700 text-white p-2 my-2 rounded-md">
+                Logout
+            </button>
         </div>
     )
 }
